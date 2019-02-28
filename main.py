68b71a6d5fe4e6058a46c4943809af79b4980738
Yuma Sumi <rbly.776324.yuma66krayt@gmail.com>
2019-02-28 20:39:55 +0900

    changed conversion function

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ face_cascade = cv2.CascadeClassifier(
 if __name__ == '__main__':
     app.run(host='127.0.0.1', threaded=True)
 
+
 def video_stream():
     global video_camera
     global global_frame
@@ -49,14 +50,10 @@ class VideoCamera(object):
             faces = face_cascade.detectMultiScale(gray, 1.3, 5)
             _, jpeg = cv2.imencode(".jpg", fra)
             for(x, y, w, h) in faces:
-                # roi = fra[y: y+h, x: x+w]
-                # hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-                # mask = cv2.inRange(hsv_roi, np.array((0., 60., 32.)), np.array((180., 255., 255.)))
-                # roi_hist = cv2.calcHist([hsv_roi], [0], mask, [180], [0,180])
-                # cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
                 now = datetime.now()
                 cv2.rectangle(fra, (x, y), (x+w, y+h), (255, 255, 255), 2)
-                cv2.putText(fra, "Face", (x, y-10), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 255), thickness=2)
+                cv2.putText(fra, "Face", (x, y-10), cv2.FONT_HERSHEY_TRIPLEX,
+                            1, (255, 255, 255), thickness=2)
                 _, jpeg = cv2.imencode(".jpg", fra)
                 _, catchFace = cv2.imencode(".jpg", fra[y:y+h, x:x+w])
                 faceList.append(catchFace.tobytes())
@@ -65,18 +62,22 @@ class VideoCamera(object):
         else:
             return None
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
-    
+
+
 @app.route('/video')
 def video():
     return Response(video_stream(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 @app.route("/photo")
 def photo():
     return jsonify(tuple(map(tuple, faceList)))
+
 
 @app.route("/photo/date")
 def date():
